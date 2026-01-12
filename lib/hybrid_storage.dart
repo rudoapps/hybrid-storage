90842a8a141c55057b458def967739030330f453
DanPerJuan <danielaperez@rudo.es>
2026-01-12 15:15:19 +0100

    Delete problems

--- lib/hybrid_storage.dart
+++ lib/hybrid_storage.dart
@@ -1,6 +1,3 @@
-/// Hybrid storage library with support for secure storage and shared preferences.
-library hybrid_storage;
-
 export 'src/source/storage_service.dart';
 export 'src/secure_storage/secure_storage_impl.dart';
 export 'src/shared_preferences/preferences_storage_impl.dart';
